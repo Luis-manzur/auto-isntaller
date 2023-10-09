dd6570873a8e303498b4300b5d825019bce1a720
Josema Calderón <49761941+josemacc@users.noreply.github.com>
2023-10-09 16:51:29 -0400

    Update update-script.sh

--- update-script.sh
+++ update-script.sh
@@ -1,19 +1,22 @@
-#!/bin/bash
+# Ejecutar git pull
+output=$(git pull)
 
-
-# Check if there are new changes
-if [[  $(git pull | grep -n "remote: Enumerating objects") ]]; then
-	echo "SI HAY NUEVA ACTUALIZACION"
-    	# Fetched new data, unzip the file
-	unzip tolls-raspberry-api_py3.9.2_release.zip
-	# Rename the unzipped folder
-	mv release-tolls-raspberry-api raspberry-machine
-	# Run a Python file
-	python raspberry-updater.py
-	# Delete the folder
-	rm -rf raspberry-machine
-	# Restart Apache server
-	sudo systemctl restart apache2
+# Verificar si la salida contiene "Enumerating objects"
+if [[ $output == *"Enumerating objects"* ]]; then
+        # Ejecutar comandos adicionales
+        echo "Actualizando..."
+        # Agrega aquí los comandos que deseas ejecutar cuando hay actualizaciones
+        echo "SI HAY NUEVA ACTUALIZACION"
+        # Fetched new data, unzip the file
+        unzip tolls-raspberry-api_py3.9.2_release.zip
+        # Rename the unzipped folder
+        mv release-tolls-raspberry-api raspberry-machine
+        # Run a Python file
+        python raspberry-updater.py
+        # Delete the folder
+        rm -rf raspberry-machine
+        # Restart Apache server
+        sudo systemctl restart apache2
 else
-	echo "NO HAY CAMBIOS"
+    echo "NO HAY ACTUALIZACIONES"
 fi
